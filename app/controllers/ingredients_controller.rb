acf72a8a57025af217b29864052e87d5696d3ad8
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-13 01:44:15 +0000

    automatically backed up by learn

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -12,6 +12,10 @@ class IngredientsController < ApplicationController
     end
   end
 
+  def show
+    @ingredient = Ingredient.find(params[:id])
+  end
+
 private
 def ingredient_params
   params.require(:ingredient).permit(:name)
