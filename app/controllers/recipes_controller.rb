b744b77e93a73174355b4753c00a39bafebc093b
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-13 02:00:58 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -13,6 +13,16 @@ class RecipesController < ApplicationController
     end
   end
 
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(recipe_params)
+    redirect_to @recipe 
+  end
+
   private
 
   def recipe_params
