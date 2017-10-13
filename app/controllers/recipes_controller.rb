655d08e2bda7622eaeec327c30329b8cef15e0b8
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-13 02:33:49 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,5 @@
 class RecipesController < ApplicationController
+
   def index
     @recipes = Recipe.all
   end
@@ -6,18 +7,18 @@ class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
   end
-
+  
   def new
     @recipe = Recipe.new
   end
 
   def create
     @recipe = Recipe.new(recipe_params)
-    if @recipe.save
-      redirect_to recipe_path(@recipe)
-    else
-      render :new
-    end
+     if @recipe.save
+       redirect_to recipe_path(@recipe)
+     else
+       render :new
+     end
   end
 
   def edit
@@ -27,11 +28,10 @@ class RecipesController < ApplicationController
   def update
     @recipe = Recipe.find(params[:id])
     if @recipe.update(recipe_params)
-
       redirect_to recipe_path(@recipe)
     else
-      render 'edit'
-    end
+     render 'edit'
+   end
   end
 
   private
@@ -39,4 +39,6 @@ class RecipesController < ApplicationController
   def recipe_params
     params.require(:recipe).permit(:name, ingredient_ids:[], ingredients_attributes:[:name])
   end
+
+
 end
