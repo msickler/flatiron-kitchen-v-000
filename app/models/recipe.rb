6b581186eeb767b008185061b701136a3a713337
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-13 02:26:09 +0000

    automatically backed up by learn

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,6 @@ class Recipe < ActiveRecord::Base
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
   validates :name, presence: true
+  validates :name, uniqueness: true 
+  accepts_nested_attributes_for :ingredients 
 end
