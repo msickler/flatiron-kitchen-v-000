bf5249ed7667dd2e3d25ac3d615c968a64da8f95
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-13 01:55:38 +0000

    automatically backed up by learn

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,17 @@
+class RecipesController < ApplicationController 
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to @recipe
+    else
+      render :new
+    end
+  end
+ 
+
+end
