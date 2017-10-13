d5d6adea4c7af9b7359cb3630ff376c64f5d4cf8
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-13 01:41:54 +0000

    automatically backed up by learn

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -3,4 +3,15 @@ class IngredientsController < ApplicationController
     @ingredient = Ingredient.new
   end
 
+  def create 
+    @ingredient = Ingredient.new(ingredient_params)
+    if @ingredient.save 
+      redirect_to @ingredient
+    else 
+      render :new 
+    end 
+  end 
+    
+   
+
 end 
