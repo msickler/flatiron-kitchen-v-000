7a3a1e7338cdb7d1c8d1ef9686ba1b62e4f55d21
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-13 01:34:10 +0000

    automatically backed up by learn

--- db/migrate/20171013012137_create_recipe_ingredients.rb
+++ db/migrate/20171013012137_create_recipe_ingredients.rb
@@ -2,7 +2,7 @@ class CreateRecipeIngredients < ActiveRecord::Migration
   def change
     create_table :recipe_ingredients do |t|
       t.integer :recipe_id
-      t.intenger :ingredient_id
+      t.integer :ingredient_id
 
       t.timestamps null: false
     end
